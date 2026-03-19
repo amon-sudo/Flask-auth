a1bc3c1c217d8aafb17df137287d91c2853beb26
amon-sudo <amonogino@gmail.com>
2026-03-19 23:09:55 +0300

    T

--- tests/__init__.py
+++ tests/__init__.py
@@ -5,3 +5,11 @@ from flask_testing import TestCase
 from src.accounts.models import User
 
 
+class BaseTetsCase(TestCase):
+    def __init__(self):
+        app.config.from_object("config.TestingConfig")
+        return app
+    def setUp(self):
+        db.create_all()
+       
+        

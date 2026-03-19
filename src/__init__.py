fc46871169104fe45118ead74615e38789126d74
amon-sudo <amonogino@gmail.com>
2026-03-19 15:18:09 +0300

    fg

--- src/__init__.py
+++ src/__init__.py
@@ -4,7 +4,7 @@ from flask_login import LoginManager
 from flask_bcrypt import Bcrypt
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
-from src.account.models import User
+from src.accounts.models import User
 
 
 app = Flask(__name__)

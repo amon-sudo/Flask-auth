971cd9d065f7f2edd4cdb36f3bd0b9212076dc05
amon-sudo <amonogino@gmail.com>
2026-03-19 23:13:33 +0300

    g

--- tests/__init__.py
+++ tests/__init__.py
@@ -11,5 +11,13 @@ class BaseTetsCase(TestCase):
         return app
     def setUp(self):
         db.create_all()
-       
+        user = User(email="ad@min.com", password="admin_user")
+        db.session.add(user)
         
+        db.session.commit()
+        
+       
+    def tearDown(self):
+        db.session.remove()
+        db.drop_all()
+        testdb_path = 
